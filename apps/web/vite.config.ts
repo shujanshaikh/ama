70a711cc7f1c67065f7625fd6c870bf89e269133
Shujan Shaikh <shaikhshujan@gmail.com>
2025-12-23 01:37:55 +0530

    fix fix

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -4,11 +4,16 @@ import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import tailwindcss from "@tailwindcss/vite";
 import viteReact from "@vitejs/plugin-react";
 import { nitro } from 'nitro/vite'
+import type { PluginOption } from "vite";
+import viteTsConfigPaths from "vite-tsconfig-paths";
 
 
 export default defineConfig({
 	server: {
 		port: 3001,
 	},
-	plugins: [tsconfigPaths(), tailwindcss(), tanstackStart(), viteReact(), nitro()],
+	plugins: [tsconfigPaths(), tailwindcss(), tanstackStart(), viteReact(), nitro() as PluginOption , 
+		viteTsConfigPaths({
+		projects: ["./tsconfig.json"],
+	  }),],
 });
