aff97a44f6e04acb9b2929ef4abcfad85d64d17f
Shujan Shaikh <shaikhshujan@gmail.com>
2025-12-26 22:45:35 +0530

    fix

--- apps/web/src/utils/constant.ts
+++ apps/web/src/utils/constant.ts
@@ -1,2 +1,2 @@
 export const API_URL : string = import.meta.env.VITE_API_URL || "http://localhost:3000/api/v1";
-export const WS_URL: string = import.meta.env.WS_URL || "ws://localhost:3000";
+export const WS_URL = import.meta.env.WS_URL;
