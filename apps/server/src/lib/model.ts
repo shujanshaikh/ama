6d654425654858a08f3bdaacddd91dcaa797617e
Shujan Shaikh <shaikhshujan@gmail.com>
2026-01-30 09:57:22 +0530

    fix:model update

--- apps/server/src/lib/model.ts
+++ apps/server/src/lib/model.ts
@@ -1,16 +1,17 @@
 import { createOpenAICompatible } from "@ai-sdk/openai-compatible";
-//import { createAnthropic } from "@ai-sdk/anthropic";
+import { createAnthropic } from "@ai-sdk/anthropic";
 import { createOpenAI } from "@ai-sdk/openai";
 import type { LanguageModel } from "ai";
 
 // Models that use /chat/completions endpoint (OpenAI-compatible)
-const OPENAI_COMPATIBLE_MODELS = ["big-pickle"];
+const OPENAI_COMPATIBLE_MODELS = ["big-pickle", "glm-4.7-free" , "kimi-k2.5-free"];
 
 // Models that use /messages endpoint (Anthropic-compatible)
-//const ANTHROPIC_MODELS = ["minimax-m2.1-free"];
+const ANTHROPIC_MODELS = ["minimax-m2.1-free"];
 
 // Models that use /responses endpoint (OpenAI)
 const OPENAI_MODELS = ["gpt-5-nano"];
+
 
 // Provider instances
 const openaiCompatibleProvider = createOpenAICompatible({
@@ -19,10 +20,10 @@ const openaiCompatibleProvider = createOpenAICompatible({
   baseURL: "https://opencode.ai/zen/v1",
 });
 
-// const anthropicProvider = createAnthropic({
-//   apiKey: process.env.OPENCODE_API_KEY,
-//   baseURL: "https://opencode.ai/zen/v1",
-// });
+const anthropicProvider = createAnthropic({
+  apiKey: process.env.OPENCODE_API_KEY,
+  baseURL: "https://opencode.ai/zen/v1",
+});
 
 const openaiProvider = createOpenAI({
   apiKey: process.env.OPENCODE_API_KEY,
@@ -37,9 +38,9 @@ export function createOpenCodeZenModel(modelId: string): LanguageModel {
     return openaiCompatibleProvider(cleanModelId);
   }
 
-  // if (ANTHROPIC_MODELS.includes(cleanModelId)) {
-  //   return anthropicProvider(cleanModelId);
-  // }
+  if (ANTHROPIC_MODELS.includes(cleanModelId)) {
+    return anthropicProvider(cleanModelId);
+  }
 
   if (OPENAI_MODELS.includes(cleanModelId)) {
     return openaiProvider(cleanModelId);
@@ -58,11 +59,23 @@ export const createMinimaxProvide = createOpenCodeZenModel;
 
 export const models = [
   {
-    id: "big-pickle",
-    name: "Big Pickle",
+    id: "kimi-k2.5-free",
+    name: "Kimi K2.5 Free",
+  },
+  {
+    id: "glm-4.7-free",
+    name: "GLM 4.7 Free",
+  },
+  {
+    id: "minimax-m2.1-free",
+    name: "Minimax M2.1 Free",
   },
   {
     id: "gpt-5-nano",
     name: "GPT 5 Nano",
+  },
+  {
+    id: "big-pickle",
+    name: "Big Pickle",
   },
 ];
